45ca306d9df0dec299c647af76650197530f78b2
hxrxchang <m.y.y.0326.cg@gmail.com>
2021-01-01 01:20:33 +0900

    feature: add branch cleaner

--- branch-cleaner.sh
+++ branch-cleaner.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+protected_branches_str=""
+
+for line in `cat ~/branch-cleaner/.protected_branches`
+do
+    if [ -z "$protected_branches_str" ]
+    then
+        protected_branches_str=$line
+    else
+        protected_branches_str="${protected_branches_str}\|${line}"
+    fi
+done
+
+git branch | grep -v $protected_branches_str | xargs git branch -D
